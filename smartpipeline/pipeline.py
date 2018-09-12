d48bcea9d66487589b015972a32c7a96cc9840ce
Giacomo Berardi <berardi@spaziodati.eu>
2018-09-13 00:07:02 +0200

    concurrent construction of stages

--- smartpipeline/pipeline.py
+++ smartpipeline/pipeline.py
@@ -1,5 +1,6 @@
 import time
 from collections import OrderedDict
+from concurrent.futures.process import ProcessPoolExecutor
 
 from smartpipeline.error import ErrorManager
 
@@ -14,8 +15,16 @@ class Pipeline:
         self._stages = OrderedDict()
         self.error_manager = ErrorManager()
         self.source = None
+        self.max_workers = None  # number of CPUs
+        self._executor = None
+
+    def _wait(self):
+        if self._executor is not None:
+            self._executor.shutdown(wait=True)
+            self._executor = None
 
     def run(self):
+        self._wait()
         if self.source is None:
             raise ValueError("Set the data source for this pipeline")
         item = self.source.pop()
@@ -24,6 +33,7 @@ class Pipeline:
             item = self.source.pop()
 
     def process(self, item):
+        self._wait()
         for name, stage in self._stages.items():
             item = self._process(stage, name, item)
             if item.has_critical_errors():
@@ -42,6 +52,10 @@ class Pipeline:
         self.error_manager = error_manager
         return self
 
+    def set_max_workers(self, max_workers):
+        self.max_workers = max_workers
+        return self
+
     def raise_on_critical_error(self):
         self._raise_on_critical = True
         return self
@@ -56,7 +70,19 @@ class Pipeline:
         return self
 
     def append_stage_concurrently(self, name, stage_class, args=[], kwargs={}, concurrency=0):
-        pass
+        self._stages[name] = None  # so the order of the calls of this method is followed
+        future = self._get_executor().submit(stage_class, args, kwargs)
+
+        def append_stage(stage_future):
+            self._stages[name] = stage_future.result()
+
+        future.add_done_callback(append_stage)
+        return self
+
+    def _get_executor(self):
+        if self._executor is None:
+            self._executor = ProcessPoolExecutor(max_workers=self.max_workers)
+        return self._executor
 
     def _process(self, stage, stage_name, item):
         try:
@@ -69,4 +95,3 @@ class Pipeline:
         # this can't be in a finally, otherwise it would register the error_manager time
         item.set_timing(stage_name, (time.time() - time1) * 1000.)
         return ret
-
