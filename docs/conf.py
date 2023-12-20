cbe03c734d54dd2ec04ea6c457d6a7f9c6695328
Giacomo Berardi <barnets@gmail.com>
2023-12-21 00:33:34 +0100

    new version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Giacomo Berardi"
 author = "Giacomo Berardi"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.2"
+release = "0.7.3"
 
 
 # -- General configuration ---------------------------------------------------
